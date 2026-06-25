68e0c23acdda890bda0f776d8cb272bae43e01a3
Krishna <rkishtiaha5@gmail.com>
2026-06-25 16:42:29 +0530

    Hop domain updated

--- infra/modules/route53/main.tf
+++ infra/modules/route53/main.tf
@@ -13,6 +13,25 @@ resource "aws_route53_record" "airflow" {
   ]
 }
 
+############################################################
+# HOP DNS
+############################################################
+
+resource "aws_route53_record" "hop" {
+
+  zone_id = aws_route53_zone.this.zone_id
+
+  name = "${var.env}-hop.${var.domain_name}"
+
+  type = "CNAME"
+
+  ttl = 300
+
+  records = [
+    var.alb_dns_name
+  ]
+}
+
 resource "aws_route53_zone" "this" {
 
   name = var.domain_name
